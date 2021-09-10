e56685188202fca4d17496be9e35ee7d0f740ad9
CarbonFodder <mahkiwi123@gmail.com>
2021-09-10 13:54:56 +0800

    Revamp Part 2

--- filemodifier.py
+++ filemodifier.py
@@ -1,4 +1,4 @@
-from os import path, sep, get_terminal_size
+from os import chdir, path, sep, get_terminal_size, rename
 from glob import iglob  # 'iglob' is better for when your working with huge directories
 from tqdm import (
     tqdm,
@@ -35,11 +35,42 @@ def display_banner():
     print(banner)
 
 
+# def reconstruct_file(file:tuple[str, str]) -> str:
+#     return
+
 # I used to make a project similar to this, and I also
 # wrote a func similar to this
 def dissect_file(file: str) -> tuple[str, str]:
+    # breakpoint()
     file = file.split(sep)[-1]
     filename, _, extension = file.partition(".")
+    exists = False
+    if extension == "":
+        extension = "File"
+    if path.exists(file):
+        exists = True
+
+    return filename, extension, exists
 
 
-dissect_file("HelloWorld")
+if __name__ == "__main__":
+    display_banner()
+
+    while True:
+        searchDirectory = path.abspath(
+            input("Enter a directory to batch-rename:\n")
+        ).replace(sep, "/")
+
+        if path.isdir(searchDirectory):
+            chdir(searchDirectory)
+            break
+
+    renameExtensions = f"Rename a certain filetype? (leave to rename every file in {searchDirectory!r})\n"
+
+    renameTo = input("Enter Output name (index will be added)")
+
+    for file in iglob("**", recursive=True):
+        file = dissect_file(file)
+        if file[2] and file[1] == renameExtensions:
+            # [ ] Get Zero Offset
+            rename(".".join(file[0:1]), "dummy")
